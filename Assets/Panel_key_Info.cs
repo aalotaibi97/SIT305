e1c6be28ae00232b97460c8da3e59a41dc1709b2
ksa_1409_8 <ksa_1409_8@hotmail.com>
2018-04-06 19:13:13 +0530

    Camera and Player Control View setup

---
+++ Assets/Panel_key_Info.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_key_Info : MonoBehaviour {
+
+
+	void OnEnable()
+	{
+		Invoke ("Hide",3.0f);
+	}
+
+	void Hide()
+	{
+		this.gameObject.SetActive (false);
+	}
+}
